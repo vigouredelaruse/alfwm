fe367ab3e5849e74d125b794e378840e0f018560
Sechelé Delaruse <ataxlab@outlook.com>
2020-06-21 20:33:29 -0400

    undoing sqlite mistake

---
+++ src/com.ataxlab.alfwm/com.ataxlab.alfwm.persistence.sqlite/IDbContextPersistenceProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ataxlab.alfwm.persistence.dbcontext
+{
+    /// <summary>
+    /// provides a mechanism for injecting your own Entity Framework types
+    /// </summary>
+    /// <typeparam name="TDbContext"></typeparam>
+    /// <typeparam name="TDbOptions"></typeparam>
+    public interface IDbContextPersistenceProvider<TDbContext, TDbOptions> 
+        where TDbContext : class  // DbContext new()
+        where TDbOptions : class // new() // DbContextOptions<TDbContext>
+    {
+        TDbContext DbContext { get; set; }
+
+        TDbOptions DbContextOptions { get; set; }
+    }
+}
