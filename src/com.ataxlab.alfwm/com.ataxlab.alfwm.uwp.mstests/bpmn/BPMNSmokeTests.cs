88bbf01d6ef8a0f415b2610c02b7922786b25afb
Sechelé Delaruse <ataxlab@outlook.com>
2020-06-29 21:26:15 -0400

    added some bpmn model classes

---
+++ src/com.ataxlab.alfwm/com.ataxlab.alfwm.uwp.mstests/bpmn/BPMNSmokeTests.cs
@@ -0,0 +1,30 @@
+﻿using com.ataxlab.alfwm.model.bpmn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.Storage;
+
+namespace com.ataxlab.alfwm.uwp.mstests.bpmn
+{
+    [TestClass]
+    public class BPMNSmokeTests
+    {
+        [TestMethod]
+        public async Task CanLoadSampleBPMN()
+        {
+            StorageFile sampleFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///samplebpmn.xml"));
+
+            var serialzer = new XmlSerializer(typeof(tDefinitions));
+            var XmlStream = new StreamReader(sampleFile.Path);
+            var document = (tDefinitions)serialzer.Deserialize(XmlStream);
+            var process = document.process;
+            var sequenceFlow = process[0].sequenceFlow[0];
+            int i = 0;
+        }
+    }
+}
