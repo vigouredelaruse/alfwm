2e853d986736b7dca63ac09f383da199c0a3637a
devopswizard <devopswizard@wizardcontroller.com>
2020-12-11 13:24:21 -0500

    ququeing httperquest pipelinetool smoketest passing sanity commit

--- src/com.ataxlab.alfwm/com.ataxlab.alfwm.library.uwp.activity/queueing/httprequest/HttpRequestQueueingActivity.cs
+++ src/com.ataxlab.alfwm/com.ataxlab.alfwm.library.uwp.activity/queueing/httprequest/HttpRequestQueueingActivity.cs
@@ -1,10 +1,15 @@
 ﻿using com.ataxlab.alfwm.core.taxonomy;
 using com.ataxlab.alfwm.core.taxonomy.pipeline;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Windows.Foundation;
 
 namespace com.ataxlab.alfwm.library.uwp.activity.queueing.httprequest
 {
@@ -17,19 +22,114 @@ namespace com.ataxlab.alfwm.library.uwp.activity.queueing.httprequest
     /// </summary>
     public class HttpRequestQueueingActivity : QueueingPipelineToolBase<HttpRequestQueueingActivityConfiguration, List<Tuple<String, String>>, HttpRequestQueueingActivityConfiguration>
     {
-        public HttpRequestQueueingActivity()
-        {
+        public override event EventHandler<PipelineToolStartEventArgs> PipelineToolStarted;
+        public override event EventHandler<PipelineToolProgressUpdatedEventArgs> PipelineToolProgressUpdated;
+        public override event EventHandler<PipelineToolFailedEventArgs> PipelineToolFailed;
+        public override event EventHandler<PipelineToolCompletedEventArgs> PipelineToolCompleted;
 
+        private HttpClient httpClient = new HttpClient();
+
+        ConcurrentQueue<HttpRequestQueueingActivityConfiguration> WorkItemCache { get; set; }
+        public Timer WorkQueueProcessTimer { get; private set; }
+
+        public HttpRequestQueueingActivity() : base()
+        {
+            WorkItemCache = new ConcurrentQueue<HttpRequestQueueingActivityConfiguration>();
+
+            WorkQueueProcessTimer = new System.Timers.Timer();
+            WorkQueueProcessTimer.AutoReset = false;
+            WorkQueueProcessTimer.Interval = 50;
+            WorkQueueProcessTimer.Elapsed += WorkQueueProcessTimer_Elapsed;
+            WorkQueueProcessTimer.Enabled = true;
+
+            this.InputBinding = new core.taxonomy.binding.QueueingChannel<HttpRequestQueueingActivityConfiguration>();
+            this.OutputBinding = new core.taxonomy.binding.QueueingChannel<List<Tuple<string, string>>>();
+
+            // enable the queue
+            this.InputBinding.IsQueuePollingEnabled = true;
+            this.InputBinding.QueueHasData += InputBinding_QueueHasData;
         }
 
+        private void InputBinding_QueueHasData(object sender, core.taxonomy.binding.queue.QueueDataAvailableEventArgs<HttpRequestQueueingActivityConfiguration> e)
+        {
+            // cache the data 
+            WorkItemCache.Enqueue(e.EventPayload);
+        }
+
+        private IAsyncAction HttpRequestAsyncAction;
+
+        private async void WorkQueueProcessTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if(WorkItemCache.Count > 0)
+            {
+                HttpRequestQueueingActivityConfiguration config;
+                var workItem = WorkItemCache.TryDequeue(out config);
+                HttpRequestAsyncAction = Windows.System.Threading.ThreadPool.RunAsync(
+                        async (state) =>
+                        {
+                            OnPipelineToolStarted(this, new PipelineToolStartEventArgs());
+                            var success = await EnsureHttpRequest(config);
+                        }
+                    );
+
+                await HttpRequestAsyncAction;
+            }
+        }
+
+        private async Task<bool> EnsureHttpRequest(HttpRequestQueueingActivityConfiguration config)
+        {
+            try
+            {
+                var request = config.RequestMessage;
+                
+                
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+
+                var content = await response.Content.ReadAsStringAsync();
+                var outMsg = new List<Tuple<String, String>>();
+                var evtMsg = new List<String>();
+                evtMsg.Add(content);
+                var outMsgPayload = new Tuple<String, String>("content", content);
+                
+                outMsg.Add(outMsgPayload);
+
+                var outTuple = new PipelineToolCompletedEventArgs<List<String>>() { Payload = evtMsg };
+
+                OnPipelineToolCompleted<List<String>>(this,outTuple);
+                this.OutputBinding.InputQueue.Enqueue(outMsg);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                OnPipelineToolFailed(this, new PipelineToolFailedEventArgs() { Status = { StatusJson = JsonConvert.SerializeObject(ex)} });
+            }
+
+            return false;        
+        }
+
+        public override void OnPipelineToolStarted(object sender, PipelineToolStartEventArgs args)
+        {
+
+            this.PipelineToolStarted?.Invoke(sender, args);
+        }
+
+        public override void OnPipelineToolProgressUpdated(object sender, PipelineToolProgressUpdatedEventArgs args)
+        {
+            this.PipelineToolProgressUpdated?.Invoke(sender, args);
+        }
+        public override void OnPipelineToolFailed(object sender, PipelineToolFailedEventArgs args)
+        {
+            this.PipelineToolFailed?.Invoke(sender, args);
+        }
         public override void OnPipelineToolCompleted<TPayload>(object sender, PipelineToolCompletedEventArgs<TPayload> args)
         {
-            throw new NotImplementedException();
+            this.PipelineToolCompleted?.Invoke(sender, new PipelineToolCompletedEventArgs() { Payload = args });
         }
 
         public override void OnQueueHasData(object sender, HttpRequestQueueingActivityConfiguration availableData)
         {
-            throw new NotImplementedException();
+            // cache the data 
+            WorkItemCache.Enqueue(availableData);
         }
 
         public override void StartPipelineTool<StartConfiguration>(StartConfiguration configuration, Action<StartConfiguration> callback)
--- src/com.ataxlab.alfwm/com.ataxlab.alfwm.uwp.mstests/QueueingPipelineTool/QueueingPipelineToolSmokeTest.cs
+++ src/com.ataxlab.alfwm/com.ataxlab.alfwm.uwp.mstests/QueueingPipelineTool/QueueingPipelineToolSmokeTest.cs
@@ -1,183 +1,202 @@
-﻿//using com.ataxlab.alfwm.core.taxonomy.binding;
-//using com.ataxlab.alfwm.core.taxonomy.pipeline;
-//using com.ataxlab.alfwm.library.activity.ocr.tesseract;
-//using com.ataxlab.alfwm.library.activity.ocr.tesseract.model;
-//using com.ataxlab.alfwm.uwp.mstests.datasetprovider.litedb.model;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Collections.Concurrent;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.InteropServices.WindowsRuntime;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using Windows.Graphics.Imaging;
-//using Windows.Storage;
-//using Windows.Storage.Streams;
+﻿using com.ataxlab.alfwm.core.taxonomy.binding;
+using com.ataxlab.alfwm.core.taxonomy.pipeline;
+using com.ataxlab.alfwm.library.uwp.activity.queueing.httprequest;
+using com.ataxlab.alfwm.uwp.mstests.datasetprovider.litedb.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
 
-//namespace com.ataxlab.alfwm.uwp.mstests.QueueingPipelineTool
-//{
-//    [TestClass]
-//    public class QueueingPipelineToolSmokeTest
-//    {
+namespace com.ataxlab.alfwm.uwp.mstests.QueueingPipelineTool
+{
+    [TestClass]
+    public class QueueingPipelineToolSmokeTest
+    {
 
-//        private ConcurrentQueue<PipelineVariable> TesseractToolWorkItemQueue { get; set; }
-//        private ConcurrentQueue<TaskItemPipelineVariable> ToolAWorkItemQueue { get; set; }
-//        private ConcurrentQueue<TaskItemPipelineVariable> ToolBWorkItemQueue { get; set; }
+        private ConcurrentQueue<PipelineVariable> TesseractToolWorkItemQueue { get; set; }
+        private ConcurrentQueue<TaskItemPipelineVariable> ToolAWorkItemQueue { get; set; }
+        private ConcurrentQueue<TaskItemPipelineVariable> ToolBWorkItemQueue { get; set; }
 
-//        [TestInitialize]
-//        public void Setup()
-//        {
-//            ToolAWorkItemQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
-//            ToolBWorkItemQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
-//            TesseractWorkQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
+        [TestInitialize]
+        public void Setup()
+        {
+            ToolAWorkItemQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
+            ToolBWorkItemQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
+            TesseractWorkQueue = new ConcurrentQueue<TaskItemPipelineVariable>();
 
-//            TesseractToolWorkItemQueue = new ConcurrentQueue<PipelineVariable>();
-//        }
+            TesseractToolWorkItemQueue = new ConcurrentQueue<PipelineVariable>();
+        }
 
-//        #region default queueing tool tests
-//        /// <summary>
-//        /// initialize a queueing pipeline tool
-//        /// post some messages to its input
-//        /// receive some processed messages on its output
-//        /// </summary>
-//        [TestMethod]
-//        public void CanPassThroughToOutputChannels()
-//        {
+        #region default queueing tool tests
+        /// <summary>
+        /// initialize a queueing pipeline tool
+        /// post some messages to its input
+        /// receive some processed messages on its output
+        /// </summary>
+        [TestMethod]
+        public void CanPassThroughToOutputChannels()
+        {
 
-//            var itemsToInsert = 10;
+            var itemsToInsert = 10;
 
-//            // initialize a tool
-//            QueueingPipelineTool<TaskItemPipelineVariable> toolA = new QueueingPipelineTool<TaskItemPipelineVariable>();
-//            // wire a listener to the tool's queue arrival event
-//            toolA.InputBinding.QueueHasData += ToolA_InputBinding_QueueHasData;
-//            toolA.InputBinding.IsQueuePollingEnabled = true;
+            // initialize a tool
+            var toolA = new QueueingPipelineTool<TaskItemPipelineVariable, QueueingPipelineToolConfiguration>();
+            // wire a listener to the tool's queue arrival event
+            toolA.InputBinding.QueueHasData += ToolA_InputBinding_QueueHasData;
+            toolA.InputBinding.IsQueuePollingEnabled = true;
 
-//            QueueingPipelineTool<TaskItemPipelineVariable> toolB = new QueueingPipelineTool<TaskItemPipelineVariable>();
-//            // wire a listener to the tool's queue arrival event
-//            toolB.InputBinding.QueueHasData += ToolB_InputBinding_QueueHasData1;
+            var toolB = new QueueingPipelineTool<TaskItemPipelineVariable, QueueingPipelineToolConfiguration>();
+            // wire a listener to the tool's queue arrival event
+            toolB.InputBinding.QueueHasData += ToolB_InputBinding_QueueHasData1;
 
-//            // wire Tool B downstream from Tool A
-//            toolA.QueueingOutputBindingCollection.Add(toolB.InputBinding);
+            // wire Tool B downstream from Tool A
+            toolA.QueueingOutputBindingCollection.Add(toolB.InputBinding);
 
-//            for (int itemNo = 0; itemNo < itemsToInsert; itemNo++)
-//            {
-//                // initialize a new pipeline tuple
-//                var newItem = this.GetNewQueueEntity(itemNo);
+            for (int itemNo = 0; itemNo < itemsToInsert; itemNo++)
+            {
+                // initialize a new pipeline tuple
+                var newItem = this.GetNewQueueEntity(itemNo);
 
-//                // post new item to Tool A input Q
-//                toolA.InputBinding.InputQueue.Enqueue(newItem);
-//            }
+                // post new item to Tool A input Q
+                toolA.InputBinding.InputQueue.Enqueue(newItem);
+            }
 
-//            // pause the test while the queue processes the inputs
-//            Thread.Sleep(15000);
+            // pause the test while the queue processes the inputs
+            Thread.Sleep(15000);
 
-//            // validate the received work item queues have the right counts
-//            Assert.IsTrue(ToolAWorkItemQueue.Count == itemsToInsert, "test failed, incorrect number of items on receive queue");
+            // validate the received work item queues have the right counts
+            Assert.IsTrue(ToolAWorkItemQueue.Count == itemsToInsert, "test failed, incorrect number of items on receive queue");
 
-//            int i = 0;
-//        }
+            int i = 0;
+        }
 
-//        private void ToolB_InputBinding_QueueHasData1(object sender, core.taxonomy.binding.queue.QueueDataAvailableEventArgs<TaskItemPipelineVariable> e)
-//        {
-//            // new dta on Tool A queue - build a private work item queue
-//            ToolBWorkItemQueue.Enqueue(e.EventPayload);
-//        }
+        private void ToolB_InputBinding_QueueHasData1(object sender, core.taxonomy.binding.queue.QueueDataAvailableEventArgs<TaskItemPipelineVariable> e)
+        {
+            // new dta on Tool A queue - build a private work item queue
+            ToolBWorkItemQueue.Enqueue(e.EventPayload);
+        }
 
-//        private void ToolA_InputBinding_QueueHasData(object sender, core.taxonomy.binding.queue.QueueDataAvailableEventArgs<TaskItemPipelineVariable> e)
-//        {
-//            ToolAWorkItemQueue.Enqueue(e.EventPayload);
-//        }
+        private void ToolA_InputBinding_QueueHasData(object sender, core.taxonomy.binding.queue.QueueDataAvailableEventArgs<TaskItemPipelineVariable> e)
+        {
+            ToolAWorkItemQueue.Enqueue(e.EventPayload);
+        }
 
-//        private TaskItemPipelineVariable GetNewQueueEntity(int i)
-//        {
-//            // initialize some test data
-//            TaskItem payload = new TaskItem()
-//            {
-//                EndTime = DateTime.UtcNow.AddDays(1),
-//                StartTimme = DateTime.UtcNow,
-//                Id = Guid.NewGuid().ToString(),
-//                TaskName = "Task Name" + i,
-//                TaskSummary = "Task Summary. This is the task summary " + i
-//            };
+        private TaskItemPipelineVariable GetNewQueueEntity(int i)
+        {
+            // initialize some test data
+            TaskItem payload = new TaskItem()
+            {
+                EndTime = DateTime.UtcNow.AddDays(1),
+                StartTimme = DateTime.UtcNow,
+                Id = Guid.NewGuid().ToString(),
+                TaskName = "Task Name" + i,
+                TaskSummary = "Task Summary. This is the task summary " + i
+            };
 
-//            TaskItemPipelineVariable queueEntity = new TaskItemPipelineVariable(payload);
-//            queueEntity.ID = Guid.NewGuid().ToString();
-//            queueEntity.TimeStamp = DateTime.UtcNow;
-//            return queueEntity;
-//        }
+            TaskItemPipelineVariable queueEntity = new TaskItemPipelineVariable(payload);
+            queueEntity.ID = Guid.NewGuid().ToString();
+            queueEntity.TimeStamp = DateTime.UtcNow;
+            return queueEntity;
+        }
 
-//        #endregion default queueing tool tests
+        #endregion default queueing tool tests
 
-//        #region tesseract tool tests
+        #region http tool tests
 
-//        [TestMethod]
-//        public async Task TesseractToolSmokeTest()
-//        {
-//            // get an image as a byte array
-//            byte[] imageAsBytes;
-//            imageAsBytes = await GetSampleImageAsBytes();
+        [TestMethod]
+        public async Task HttpToolSmokeTest()
+        {
+            // get an image as a byte array
+            byte[] imageAsBytes;
+            imageAsBytes = await GetSampleImageAsBytes();
 
-           
-//            int i = imageAsBytes.Length;
 
-//            // initialize the tesseract tool
-//            PipelineVariable testTuple = new PipelineVariable();
-//            testTuple.Payload = imageAsBytes;
+            int i = imageAsBytes.Length;
 
-//            TesseractActivity activity = new TesseractActivity();
-//            activity.QueueHasAvailableDataEvent += Activity_QueueHasAvailableDataEvent;
-//            activity.InputBinding.IsQueuePollingEnabled = true;
+            // initialize the tesseract tool
+            PipelineVariable testTuple = new PipelineVariable();
+            testTuple.Payload = imageAsBytes;
 
-//            activity.InputBinding.InputQueue.Enqueue(testTuple);
+            var activity = new HttpRequestQueueingActivity();
+            activity.QueueHasAvailableDataEvent += Activity_QueueHasAvailableDataEvent1;
+            activity.InputBinding.IsQueuePollingEnabled = true;
 
-//            Thread.Sleep(10000);
+            var activityConfig = new HttpRequestQueueingActivityConfiguration();
+            activityConfig.RequestMessage = new System.Net.Http.HttpRequestMessage() { Method = HttpMethod.Get, RequestUri = new Uri("https://www.cnn.com") };
+            activity.PipelineToolCompleted += Activity_PipelineToolCompleted;
+            activity.InputBinding.InputQueue.Enqueue(activityConfig);
 
-//            Assert.IsTrue(activity.InputBinding.InputQueue.Count > 0, "test failed, input queue count incorrect");
-//        }
+            Thread.Sleep(10000);
 
-//        ConcurrentQueue<TaskItemPipelineVariable> TesseractWorkQueue { get; set; }
 
-//        private PipelineVariable Activity_QueueHasAvailableDataEvent(PipelineVariable arg)
-//        {
-//            PipelineVariable v = new PipelineVariable();
-//            return v;
-//        }
 
-//        private static async Task<byte[]> GetSampleImageAsBytes()
-//        {
-//            SoftwareBitmap softwareBitmap;
-//            byte[] imageAsBytes;
+            Assert.IsTrue(didfirePipelineToolCompleted, "test failed, did not fire activity completed event");
+        }
 
-//            Windows.Storage.StorageFolder storageFolder =
-//            Windows.Storage.ApplicationData.Current.LocalFolder;
+        private void Activity_PipelineToolCompleted(object sender, core.taxonomy.PipelineToolCompletedEventArgs e)
+        {
+            didfirePipelineToolCompleted = true;
+        }
 
-//            StorageFile sampleFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///sample.png"));
+        bool didFireQueueHasAvailableDataEvent = false;
+        bool didfirePipelineToolCompleted = false;
+        private HttpRequestQueueingActivityConfiguration Activity_QueueHasAvailableDataEvent1(HttpRequestQueueingActivityConfiguration arg)
+        {
+            Assert.IsNotNull(arg, "test failed: queue arrival data");
+            this.didFireQueueHasAvailableDataEvent = true;
+            return arg;
+        }
 
-//            //Windows.Storage.StorageFile sampleFile =
-//            //    await storageFolder.GetFileAsync("sample.png");
+        ConcurrentQueue<TaskItemPipelineVariable> TesseractWorkQueue { get; set; }
 
-//            using (IRandomAccessStream stream = await sampleFile.OpenAsync(FileAccessMode.Read))
-//            {
-//                // Create the decoder from the stream
-//                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+        private PipelineVariable Activity_QueueHasAvailableDataEvent(PipelineVariable arg)
+        {
+            PipelineVariable v = new PipelineVariable();
+            return v;
+        }
 
-//                // Get the SoftwareBitmap representation of the file
-//                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+        private static async Task<byte[]> GetSampleImageAsBytes()
+        {
+            SoftwareBitmap softwareBitmap;
+            byte[] imageAsBytes;
 
-//                var re = softwareBitmap.BitmapPixelFormat;
-//                uint size = (uint)softwareBitmap.PixelHeight * (uint)softwareBitmap.PixelWidth;
-//                Windows.Storage.Streams.Buffer buffer = new Windows.Storage.Streams.Buffer(size * 32);
-//                softwareBitmap.CopyToBuffer(buffer);
+            Windows.Storage.StorageFolder storageFolder =
+            Windows.Storage.ApplicationData.Current.LocalFolder;
 
-//                imageAsBytes = buffer.ToArray(0, (int)buffer.Length);
+            StorageFile sampleFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///sample.png"));
 
-//            }
+            //Windows.Storage.StorageFile sampleFile =
+            //    await storageFolder.GetFileAsync("sample.png");
 
-//            return imageAsBytes;
-//        }
+            using (IRandomAccessStream stream = await sampleFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
 
-//        #endregion tesseract tool tests
-//    }
-//}
+                // Get the SoftwareBitmap representation of the file
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+                var re = softwareBitmap.BitmapPixelFormat;
+                uint size = (uint)softwareBitmap.PixelHeight * (uint)softwareBitmap.PixelWidth;
+                Windows.Storage.Streams.Buffer buffer = new Windows.Storage.Streams.Buffer(size * 32);
+                softwareBitmap.CopyToBuffer(buffer);
+
+                imageAsBytes = buffer.ToArray(0, (int)buffer.Length);
+
+            }
+
+            return imageAsBytes;
+        }
+
+        #endregion http tool tests
+    }
+}
