46af6276bddf14cf46ba5289c7c12a0ae82f32d6
devopswizard <devopswizard@wizardcontroller.com>
2021-01-17 14:36:15 -0500

    scaffolding more builders

---
+++ src/com.ataxlab.alfwm/com.ataxlab.alfwm.core/taxonomy/deployment/queueing/DefaultQueueingPipelineNodeDeploymentContainerBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.ataxlab.alfwm.core.taxonomy.deployment.queueing
+{
+    public class DefaultQueueingPipelineNodeDeploymentContainerBuilder
+    {
+        private DefaultQueueingPipelineNodeDeploymentContainer container;
+
+        public DefaultQueueingPipelineNodeDeploymentContainerBuilder()
+        {
+
+        }
+
+    }
+}
